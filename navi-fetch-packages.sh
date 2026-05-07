c8b80427239413a9f1811682037942b946ca93d4
Prudviteja99 <prudvitejabandam987@gmail.com>
2026-05-07 06:33:48 +0530

    fix: use eval for command execution to correctly handle pipes
    This fixes errors where shell operators in command variables were treated as literal arguments, specifically resolving 'repository not found' errors when fetching AUR packages.
    
    Also added 'refresh' as a valid full-word action input.

--- navi-fetch-packages.sh
+++ navi-fetch-packages.sh
@@ -24,28 +24,28 @@ fetch_remote_packages(){
 
 
 fetch_pacman_installed_packages(){
-    $PACMAN_QUERY_CMD > "$PACMAN_INSTALLED_TMP" &
+    eval "$PACMAN_QUERY_CMD" > "$PACMAN_INSTALLED_TMP" &
     PACMAN_INSTALLED_PID=$!
 }
 fetch_aur_installed_packages(){
-    $AUR_QUERY_CMD > "$AUR_INSTALLED_TMP" &
+    eval "$AUR_QUERY_CMD" > "$AUR_INSTALLED_TMP" &
     AUR_INSTALLED_PID=$!
 }
 fetch_flatpak_installed_packages(){
-    $FLATPAK_QUERY_CMD > "$FLATPAK_INSTALLED_TMP" &
+    eval "$FLATPAK_QUERY_CMD" > "$FLATPAK_INSTALLED_TMP" &
     FLATPAK_INSTALLED_PID=$!
 }
 
 
 fetch_pacman_remote_packages(){
-    $PACMAN_REMOTE_CMD > "$PACMAN_REMOTE_TMP" &
+    eval "$PACMAN_REMOTE_CMD" > "$PACMAN_REMOTE_TMP" &
     PACMAN_REMOTE_PID=$!
 }
 fetch_aur_remote_packages(){
-    $AUR_REMOTE_CMD > "$AUR_REMOTE_TMP" &
+    eval "$AUR_REMOTE_CMD" > "$AUR_REMOTE_TMP" &
     AUR_REMOTE_PID=$!
 }
 fetch_flatpak_remote_packages(){
-    $FLATPAK_REMOTE_CMD > "$FLATPAK_REMOTE_TMP" &
+    eval "$FLATPAK_REMOTE_CMD" > "$FLATPAK_REMOTE_TMP" &
     FLATPAK_REMOTE_PID=$!
 }
